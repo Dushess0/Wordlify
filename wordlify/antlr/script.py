02c4a65fe3532e626ed9bc2a2922e2923b7ce161
Dushess0 <ilyabannila@gmail.com>
2021-04-29 12:36:02 +0200

    extended example of dwuprzebiegowośc

--- wordlify/antlr/script.py
+++ wordlify/antlr/script.py
@@ -1,5 +1,14 @@
+import urllib.request
+
 def a(b):
     print(b)
+
+def download_algorhitm(url):
+    filename = "my_file.zip"
+    try:
+        urllib.request.urlretrieve(url, filename)
+    except Exception as v0:
+        print(str(v0))
 
 a(5)
 d = 5
@@ -9,3 +18,8 @@ if test == 5:
 else:
     test = "error.txt"
 print(test)
+condition = 0
+url = "https://github.com/Dushess0/Wordlify/archive/refs/heads/master.zip"
+if condition == 0:
+    url = "https://raw.githubusercontent.com/Dushess0/Wordlify/master/kalkulator/Wordlify.g4"
+download_algorhitm(url)
--- wordlify/antlr/script.wl
+++ wordlify/antlr/script.wl
@@ -6,3 +6,21 @@ d = 5
 test = 5 
 if test == 5 then test = "file.txt" else test="error.txt" end
 print(test)
+
+
+
+
+
+condition=0
+url="https://github.com/Dushess0/Wordlify/archive/refs/heads/master.zip"
+if condition==0 then url = "https://raw.githubusercontent.com/Dushess0/Wordlify/master/kalkulator/Wordlify.g4" end
+
+
+download_algorhitm(url)
+
+
+fn download_algorhitm(url)
+begin 
+filename="my_file.zip"
+download(url,filename)
+end
