eb9861527319da7234e700163ff1c047925d1e5f
Dushess0 <ilyabannila@gmail.com>
2021-06-10 10:24:54 +0200

    improved error handling

--- wordlify/antlr/compiler.py
+++ wordlify/antlr/compiler.py
@@ -10,6 +10,37 @@ from FnListener import FnListener
 import os
 import io
 
+
+def process_error(content):
+    full_content= "".join(content)
+    brackets={"[":0,"]":0,"(":0,")":0}
+    for char in full_content:
+        if char.lower() in brackets.keys():
+                brackets[char]+=1
+
+
+    for item in brackets.keys():
+        if item=="[" and brackets["["]!=brackets["]"]:
+            print("Possible cause: unpaired bracket")
+        if item=="(" and brackets["("]!=brackets[")"]:
+            print("Possible cause: unpaired bracket")
+
+
+
+    keywords={"while":0,"foreach":0,"for":0,"do":0,"end":0,"in":0,"if":0,"else":0,"then":0,"begin":0}
+    for word in full_content.split():
+        if word.lower() in keywords.keys():
+                keywords[word.lower()]+=1
+    if keywords["foreach"] + keywords["while"] > keywords["do"]:
+        print("There is missed 'do' in foreach/while statement")
+    if keywords["foreach"] + keywords["while"] < keywords["do"]:
+        print("'do' used without foreach/while statement")
+    if keywords["if"] > keywords["then"]:
+        print("There is missed 'then' after 'if' statement")
+    if keywords["if"] < keywords["then"]:
+        print("'then' used without 'if' statement ")
+
+
 def main(argv):
     if len(argv) != 2 or argv[1][-3:] != ".wl":
         print("Pass *.wl file as parameter")
@@ -32,6 +63,7 @@ def main(argv):
         if msg != "":
             errorDetails = msg.split(",")
             print("Line {}, column {}: unexpected symbol '{}'\n    {}".format(errorDetails[0], errorDetails[1], errorDetails[2], src_lines[int(errorDetails[0])-1].lstrip()))
+            process_error(src_lines)
             quit()
         
         destFileName = argv[1][:-2] + "py"
